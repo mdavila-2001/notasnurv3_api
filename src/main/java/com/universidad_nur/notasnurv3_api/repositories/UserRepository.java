47dded075b1f7909bd976adfd2dce3e13246655e
Fernando.T <fernandoterrzasfiligrana@gmail.com>
2026-04-12 19:39:11 -0400

    feat: registro de usuarios funcional con procedimiento almacenado y db personal

--- src/main/java/com/universidad_nur/notasnurv3_api/repositories/UserRepository.java
+++ src/main/java/com/universidad_nur/notasnurv3_api/repositories/UserRepository.java
@@ -1,27 +1,34 @@
 package com.universidad_nur.notasnurv3_api.repositories;
 
-import com.universidad_nur.notasnurv3_api.entities.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.query.Procedure;
-import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.universidad_nur.notasnurv3_api.entities.Role;
+import com.universidad_nur.notasnurv3_api.entities.Users;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, UUID> {
 
+    List<Users> findByRole(Role role);
+
 @Procedure(procedureName = "pr_create_user")
 void createNewUser(
-        String p_name,
-        String p_middle_name,
-        String p_last_name,
-        String p_mother_last_name,
-        String p_email,
-        String p_pass,
-        String p_role
+    @Param("p_ci") String p_ci,
+    @Param("p_first_name") String p_name,
+    @Param("p_middle_name") String p_mid,
+    @Param("p_last_name_paternal") String p_lp,
+    @Param("p_last_name_maternal") String p_lm,
+    @Param("p_email") String p_email,
+    @Param("p_pass") String p_pass,
+    @Param("p_role") String p_role
 );
 
-Optional<Users> findByEmail(String email);
-Optional<Users> findByCi(String ci);
+    Optional<Users> findByEmail(String email);
+    Optional<Users> findByCi(String ci);
 }
